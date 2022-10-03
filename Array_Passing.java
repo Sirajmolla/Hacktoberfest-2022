1c67f0eb0e3d6cabfa6069cda6467565418f788f
Poulami Bera <65413328+Poulami769988@users.noreply.github.com>
2022-10-03 14:10:04 +0530

    Array Passing Using Java

---
+++ Array_Passing.java
@@ -0,0 +1,17 @@
+
+public class Array_Passing {
+
+	static void display(int arr[])
+	{
+		for(int i:arr)
+		{
+			System.out.print(i+" ");
+		}
+	}
+	public static void main(String[] args) {
+		int arr[]= {10,20,30,40,50};
+		display(arr);
+		
+	}
+
+}
