e365de332698f1e9196d4e9d6ef2b579cf5826e3
BhavneeshOG <bhavneesh85@gmail.com>
2022-10-10 22:36:10 +0530

    updated

--- C++/bfs.cpp
+++ C++/bfs.cpp
@@ -1,16 +1,45 @@
-//Breadth-First Travaersal of a graph represented by adjacency matrix
-void bfs(vector<int> adj[], int v, int start) {
-    vector<bool> visited(v, false);
-    queue<int> q;
-    q.push(start);
-    visited[start] = true;
-    
-    while(!q.empty()) {
-        int x = q.front();
-        q.pop();
-        cout << x << " ";
-        for(int y:adj[x]) {
-            if(!visited[y]) {visited[y]=true;q.push(y);}
-        }
-    }
+#include<bits/stdc++.h>
+using namespace std;
+
+void bfs(vector<vector<int>> &adjList,int source,int N){
+	vector<bool> visited(N,false);
+	
+	queue<int> q;
+	visited[source]=true;
+	q.push(source);
+	
+	while(!q.empty()){
+		int currentNode=q.front();
+		cout<<"Visited: "<<currentNode<<endl;
+		q.pop();
+		int totalNeighbour = adjList[currentNode].size();
+		
+		for(int i=0;i<totalNeighbour;i++){
+			int neighbourNode = adjList[currentNode][i];
+			
+			if(visited[neighbourNode]==false){
+				visited[neighbourNode]=true;
+				q.push(neighbourNode);
+			}
+		}
+	}
+}
+
+int main(){
+	
+	int N=6;
+	
+	vector<vector<int>> adjList(N);
+	
+	adjList[0].push_back(1);
+	adjList[0].push_back(2);
+	adjList[0].push_back(4);
+	
+	adjList[1].push_back(3);
+	adjList[2].push_back(3);
+	adjList[3].push_back(4);
+	adjList[3].push_back(5);
+	
+	bfs(adjList,0,N);
+	
 }
